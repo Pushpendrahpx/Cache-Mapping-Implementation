99334c6dc44532efcd655fd07952da279526bf0d
Pushpendra Vishwakarma <pushpendra.hpx2001@gmail.com>
2020-11-23 17:15:58 +0000

    Transfer Function Done

--- main.cpp
+++ main.cpp
@@ -21,13 +21,69 @@ BLOCK MAIN[MAIN_MEMORY_SIZE] = {0};
 BLOCK CACHE[CACHE_MEMORY_SIZE];
 
 
+
+
+
+// choice = 0, means, Data Transfer will be from MAIN_MEMORY to CACHE_MEMORY
+// choice = 1, means, Data Transfer will be from CACHE_MEMORY to MAIN_MEMORY
+// ##### NOTE - No Matter we transfer data from AnyWhere to Anywhere, whole Word should be transfered. #####
+
+// [ No Issues of Bandwidth will be there while Tranferring as whole word will be get transferred ]
+void transfer(int main_block,int main_word,int cache_block, int cache_word,int choice = 0){
+
+    if(choice == 0){
+        // Main => Cache Memory
+        for(int by = 0; by < WORD_SIZE; by++){
+            for(int bi = 0;  bi < BYTE_SIZE ; bi++){
+                
+                CACHE[bl][wo][by][bi];
+                
+            }
+        }
+    }else{
+        // Cache ==> Main_Memory
+        for(int by = 0; by < WORD_SIZE; by++){
+            for(int bi = 0;  bi < BYTE_SIZE ; bi++){
+                
+                cout<<CACHE[bl][wo][by][bi];
+            }
+        }
+    }
+
+
+}
+
+
+
 // This Functions Accepts Parameter, how many Blocks of Main Memory we want to see.
 // Default parameters are used as 1, if function is called without parameter loop will not execute, and no output will be generated
-// So Make Sure to pass some no. 
+// So Make Sure to pass some no.
+void see_cacheMemory(int to_count = 1){
+    if(to_count <= 8){
+        // TO Ensure Input Number is With in Limits
+
+        cout<<"\n\t\t CACHE MEMORY MATRIX \t\t\n";
+        for(int bl = 0; bl < to_count ; bl++){
+            for(int wo = 0; wo < BLOCK_SIZE; wo++){
+                for(int by = 0; by < WORD_SIZE; by++){
+                    for(int bi = 0;  bi < BYTE_SIZE ; bi++){
+                        cout<<CACHE[bl][wo][by][bi];
+                    }
+                    cout<<" | ";
+                }
+                cout<<"\n";
+            }
+            cout<<"\n\n";
+        }
+    }
+}
+// This Functions Accepts Parameter, how many Blocks of Main Memory we want to see.
+// Default parameters are used as 1, if function is called without parameter loop will not execute, and no output will be generated
+// So Make Sure to pass some no.
 void see_mainMemory(int to_count = 1){
     if(to_count <= 128){
         // TO Ensure Input Number is With in Limits
-        
+
         cout<<"\n\t\t MAIN MEMORY MATRIX \t\t\n";
         for(int bl = 0; bl < to_count ; bl++){
             for(int wo = 0; wo < BLOCK_SIZE; wo++){
@@ -49,10 +105,10 @@ int main(){
         cout<<" ~[CACHE] $ ";
         cin>>MAIN[0][0][0][1];
 
-        see_mainMemory();
-        
-        
-        
+        see_cacheMemory();
+
+
+
         cout<<"\n Type 'N' to Exit or any other key to Continue :- ";
         cin>>choice;
          cout<<"\n";
